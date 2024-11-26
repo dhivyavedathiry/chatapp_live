4081d243b99eb8fafd060de745f27819ad9a6089
Dhivya <srisubramanian2012@gmail.com>
2024-11-26 17:05:10 +0530

    settime

--- controllers/signupcontroller.js
+++ controllers/signupcontroller.js
@@ -45,4 +45,30 @@ function generateAccessToken(id, name, ispremiumuser) {
     return jwt.sign({ userId: id, name: name, ispremiumuser }, 'secretkey')
 };
 
+
+exports.userLogin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ where: { email: email } });
+
+        //case1 - if the emailId is incorrect
+        if (!user) {
+            return res.status(404).json({ error: "Incorrect email, User not found", success: false });
+        }
+
+        //case2 if the emailId is correct
+        const match = await bcrypt.compare(password, user.password);  //matching the entered password with the hashed password stored in database
+        if (match) {
+            return res.status(200).json({ data: user, message: "User logged in successfully", success: true, token: generateAccessToken(user.id, user.name) });
+
+        } else {
+            return res.status(401).json({ error: "Incorrect password", success: false });
+        }
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ error: "Internal server error" });
+    }
+}
+
 exports.generateAccessToken = generateAccessToken;
